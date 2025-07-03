ebe4efed0d10da98131dd5f96f5bc936f3f02e0c
yb2527 <yb2527@swu.ac.kr>
2025-07-03 15:58:20 +0900

    feat: 게시글 상세페이지 구현

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+    path('<int:post_id>/', views.post_detail, name='post_detail'),  # 클릭한 글 상세페이지
+    path('<slug:category_slug>/', views.post_list, name='post_list_by_category'),  # 선택한 카테고리 게시글만 표시
     path('', views.post_list, name='post_list'),
-    path('<slug:category_slug>/', views.post_list, name='post_list_by_category'),  # 선택한 카테고리 게시글만
 ]
--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,19 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'username',
+        'email',
+        'is_doctor',
+        'hospital',
+        'phone_number',
+        'date_of_birth',
+        'created_at',
+    )
+
+    search_fields = ('username', 'email', 'hospital')
+
+    list_filter = (
+        'is_doctor',
+    )
+    ordering = ('-created_at',)
