8f3b9d56aee9b7c2470ad4ccfb2bc5c4923e25d6
ss0yaa <syp0205@sookmyung.ac.kr>
2025-07-05 22:53:37 +0900

    병원 검색 ui

--- hospital/urls.py
+++ hospital/urls.py
@@ -6,5 +6,6 @@ from .views import HospitalListAPIView, hospital_list
 urlpatterns = [
     path('hospital_list/', views.hospital_list, name='hospital_list'),
     path('api/hospitals/', HospitalListAPIView.as_view(), name='hospital_list_api'),
-    path('review/create/', views.review_create, name='review_create'),
-]
+    # 임시 작성
+    path('hospital_search/', views.hospital_search, name='hospital_search'),
+    path('review/create/', views.review_create, name='review_create'),]
