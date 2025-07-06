210f09adc25b562487eb555c98012655f858e0e9
ss0yaa <syp0205@sookmyung.ac.kr>
2025-07-06 22:25:15 +0900

    병원 정보 페이지 구현

--- hospital/views.py
+++ hospital/views.py
@@ -84,3 +84,7 @@ def hospital_search(request):
 # 리뷰 작성 (임시)
 def review_create(request):
     return render(request, 'hospital/review_create.html')
+
+# 병원 상세 정보 (임시)
+def hospital_detail(request):
+    return render(request, 'hospital/hospital_detail.html')
