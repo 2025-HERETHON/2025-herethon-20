1b9b4ce198b7fd97b0d35be35535c5a1b73b663f
heveidndksjd <kayoonkim@naver.com>
2025-07-12 02:51:09 +0900

    feat: 전문의 검색 기능

--- hospital/urls.py
+++ hospital/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('hospital_reviews/<int:hospital_id>/', views.hospital_reviews, name='hospital_reviews'),
     path('hospital_reviews/<int:hospital_id>/review_search/', views.review_search, name='review_search'),
     path('hospital_reserve_search/', views.hospital_reserve_search, name='hospital_reserve_search'),
+    path('hospital_reserve/', views.hospital_reserve, name='hospital_reserve'),
     path('my_reviews/', views.my_reviews, name='my_reviews'),
     path('my_page/', views.my_page, name='my_page'),
 ]
